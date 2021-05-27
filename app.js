8a1b93d65ca68909055bba35b615f7487d5bf368
sophiamf93 <sophia_mf@icloud.com>
2021-05-27 14:31:18 -0500

    second commit

--- app.js
+++ app.js
@@ -21,42 +21,56 @@ const friends = [
 ]
 
 let body = document.body;
-    let buttonCreate = document.createElement("button");
-    let buttonText = document.createTextNode("Sing!");
+let buttonCreate = document.createElement("button");
+let buttonText = document.createTextNode("Sing!");
 
-    window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", function () {
     buttonCreate.appendChild(buttonText);
     body.appendChild(buttonCreate);
-    }
-    )
-
-    buttonCreate.addEventListener("click", function () {
-        
-    }
-
-for (let i = 0; i < friends.length; i++) {
-    console.log(friends[i].name);
-
-    for (let song = 99; song > 0; song--) {
-        j = song - 1;
-        if (song != 1) {
-            wordLine = "lines";
-        } else {
-            wordLine = "line";
-        }
-        if (j != 1) {
-            wordLine2 = "lines";
-        } else {
-            wordLine2 = "line";
-        }
-        console.log(song + " " + wordLine + " of code in the file,");
-        console.log(song + " " + wordLine + " of code,");
-        console.log(friends[i].name + " " + "strikes one out, clears it all out,");
-        if (j != 0) {
-            console.log(j + " " + wordLine2 + " of code in the file.");
-        } else {
-            console.log("No more lines of code in the file!");
-        }
-    }
-
 }
+)
+
+let FriendDiv = document.createElement("div");
+FriendDiv.setAttribute("class", "friend");
+body.appendChild(FriendDiv);
+
+buttonCreate.addEventListener("click", function () {
+    friends.forEach(function (name, index) {
+        let FriendHeader = document.createElement("h3");
+
+        FriendHeader.innerText = name.name;
+        FriendDiv.appendChild(FriendHeader);
+        // FriendDiv.appendChild(FriendParagraph);
+
+        for (let song = 99; song > 0; song--) {
+            let FriendParagraph = document.createElement("p");
+
+            j = song - 1;
+            wordLine = "lines";
+            wordLine2 = "line";
+
+            if (song !== 2 && song !== 1) {
+
+                FriendParagraph.innerHTML = song + " " + wordLine + " of code in the file, " + song + " " + wordLine + " of code, " +
+                    name.name + " strikes one out, clears it all out, " + j + " " +
+                    wordLine + " of code in the file.";
+            } else if (song == 2) {
+                FriendParagraph.innerHTML = song + " " + wordLine + " of code in the file, " + song + " " + wordLine + " of code," +
+                    name.name + " " + "strikes one out, clears it all out, " + j + " " +
+                    wordLine2 + " of code in the file.";
+            } else if (song == 1) {
+                FriendParagraph.innerHTML = song + " " + wordLine2 + " of code in the file, " + song + " " + wordLine2 + " of code," +
+                    name.name + " " + "strikes one out, clears it all out, " + "no more " + " " +
+                    wordLine + " of code in the file.";
+
+            }
+
+            FriendDiv.appendChild(FriendParagraph);
+
+        }
+
+    })
+
+})
+
+
